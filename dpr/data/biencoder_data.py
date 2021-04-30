9a649201e8e0f3d3d2ca595bf53f9e16ff6aed26
jscarlson <jcarlson238@gmail.com>
2021-04-30 01:03:05 -0400

    Custom MNLI-based training

--- dpr/data/biencoder_data.py
+++ dpr/data/biencoder_data.py
@@ -196,6 +196,81 @@ class JsonQADataset(Dataset):
         )
 
 
+class CustomJsonQADataset(Dataset):
+    def __init__(
+        self,
+        file_path: str,
+        selector: DictConfig = None,
+        special_token: str = None,
+        encoder_type: str = None,
+        shuffle_positives: bool = False,
+        normalize: bool = False,
+        query_special_suffix: str = None,
+    ):
+        super().__init__(
+            selector,
+            special_token=special_token,
+            encoder_type=encoder_type,
+            shuffle_positives=shuffle_positives,
+            query_special_suffix=query_special_suffix,
+        )
+        self.file_path = file_path
+        self.data_files = []
+        self.data = []
+        self.normalize = normalize
+        logger.info("Data files: %s", self.data_files)
+
+    def load_data(self):
+        data = read_data_from_json_files([self.file_path])
+        # filter those without positive ctx
+        self.data = [r for r in data if len(r["positive_ctxs"]) > 0]
+        logger.info("Total cleaned data size: {}".format(len(self.data)))
+
+    def __getitem__(self, index) -> BiEncoderSample:
+        json_sample = self.data[index]
+        r = BiEncoderSample()
+        r.query = self._process_query(json_sample["question"])
+
+        positive_ctxs = json_sample["positive_ctxs"]
+        negative_ctxs = (
+            json_sample["negative_ctxs"] if "negative_ctxs" in json_sample else []
+        )
+        hard_negative_ctxs = (
+            json_sample["hard_negative_ctxs"]
+            if "hard_negative_ctxs" in json_sample
+            else []
+        )
+
+        for ctx in positive_ctxs + negative_ctxs + hard_negative_ctxs:
+            if "title" not in ctx:
+                ctx["title"] = None
+
+        def create_passage(ctx: dict):
+            return BiEncoderPassage(
+                normalize_passage(ctx["text"]) if self.normalize else ctx["text"],
+                ctx["title"],
+            )
+
+        r.positive_passages = [create_passage(ctx) for ctx in positive_ctxs]
+        r.negative_passages = [create_passage(ctx) for ctx in negative_ctxs]
+        r.hard_negative_passages = [create_passage(ctx) for ctx in hard_negative_ctxs]
+        return r
+
+    def __len__(self):
+        return len(self.data)
+
+    def get_qas(self) -> Tuple[List[str], List[str]]:
+        return [s["question"] for s in self.data], [s["answers"] for s in self.data]
+
+    def get_qas_range(
+        self, start_idx: int, end_idx: int
+    ) -> Tuple[List[str], List[str]]:
+        return (
+            [s["question"] for s in self.data[start_idx:end_idx]],
+            [s["answers"] for s in self.data[start_idx:end_idx]],
+        )
+
+
 def normalize_passage(ctx_text: str):
     ctx_text = ctx_text.replace("\n", " ").replace("’", "'")
     return ctx_text
