38a4db2d2a9f815080a924d821cd7f0e4e6f9d51
jscarlson <jcarlson238@gmail.com>
2021-05-07 03:34:03 -0400

    Assess RoBERTa

--- dpr/models/hf_models.py
+++ dpr/models/hf_models.py
@@ -183,7 +183,7 @@ def get_roberta_tokenizer(pretrained_cfg_name: str, do_lower_case: bool = True):
     )
 
 
-class HFBertEncoder(BertModel):
+class HFBertEncoder(RobertaModel):
     def __init__(self, config, project_dim: int = 0):
         super().__init__(config)
         assert config.hidden_size > 0, "Encoder hidden_size can't be zero"
