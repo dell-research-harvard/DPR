7e56833ad4908d15a2b89a6813cf0176b34dfdb3
Emily Silcock <emilysilcock@gmail.com>
2021-10-16 18:09:12 +0200

    Add test file to work out why it's struggling with bart-large

--- test.py
+++ test.py
@@ -5,4 +5,4 @@ classifier = pipeline("zero-shot-classification", model=model, device=0)
 
 candidate_labels = ["Vietnam war", "Other topic"]
 
-classifier("I like pasta", candidate_labels)
+print(classifier("I like pasta", candidate_labels))
