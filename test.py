93c063e707a4bd34051e5af41834161a5eb7ad86
Emily Silcock <emilysilcock@gmail.com>
2021-10-16 18:14:11 +0200

    Change batch size

--- test.py
+++
@@ -1,8 +0,0 @@
-from transformers import pipeline
-
-model = "facebook/bart-large"
-classifier = pipeline("zero-shot-classification", model=model, device=0)
-
-candidate_labels = ["Vietnam war", "Other topic"]
-
-print(classifier("I like pasta", candidate_labels))
