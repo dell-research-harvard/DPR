6e8687959d47bf95522766fb6177b8bba50dac8c
Emily Silcock <emilysilcock@gmail.com>
2021-10-25 10:48:35 -0400

    Change output fields

--- test.py
+++ test.py
@@ -22,7 +22,7 @@ class DBSolr:
         for doc in tqdm(self.solr.search(query, fl='id,article,headline', sort='id ASC', cursorMark='*')):
             ids.extend(doc['id'])
             articles.extend(doc['article'])
-            headlines.extend(doc['headline'])
+            #headlines.extend(doc['headline'])
 
         self.ids = ids
         self.articles = articles
