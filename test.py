c58e6c09e392154eefbea3c9c3caf3b1c7d9e288
Emily Silcock <emilysilcock@gmail.com>
2021-10-25 11:06:14 -0400

    Change output

--- test.py
+++ test.py
@@ -43,4 +43,4 @@ if __name__ == '__main__':
     db.gather_ocr_texts_and_metadata(query='headline:"senate" AND (article:"pill" OR article:"oral" OR '                # Random small search
                                            'article:"contracepti") AND image_file_name:"-1968"')
 
-    print(db.ids)
+    print(db.headlines)
